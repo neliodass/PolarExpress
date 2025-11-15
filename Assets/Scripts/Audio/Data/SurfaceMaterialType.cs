8bf31495632aaa06bdb4b2da8e5faf25e6795403
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2025-11-15 02:36:49 +0100

    Add sounds
    add sounds and scriptable data files for walk and sprint on wood metal and default surfaces

---
+++ Assets/Scripts/Audio/Data/SurfaceMaterialType.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Audio.Data
+{
+    [CreateAssetMenu(fileName = "Surface_New", menuName = "Audio/Surface Material Type")]
+    public class SurfaceMaterialType: ScriptableObject
+    {
+        
+    }
+}
